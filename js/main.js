a12c0fe65bacb8d9d570799b48ad6a8b5dd3e583
Léna Faure <lena.faure@gmail.com>
2017-01-27 15:52:29 +0100

    Working on price display

--- js/main.js
+++ js/main.js
@@ -1,65 +1,69 @@
+var shoppingCart = document.getElementById("cart");
+var addToCart = document.querySelectorAll(".add-to-cart");
+var hideShoppingCart = document.getElementById("hide-shopping-cart");
+var headerCartButton = document.getElementById("cart-header-button");
+var cartItemsWrapper = document.querySelector(".cart-items-wrapper");
+var emptyCartMsg = document.getElementById("empty-cart");
 
-window.onload = function() {
+var plusButton = document.querySelectorAll(".plusButton");
 
-    var shoppingCart = document.getElementById("cart");
-    var addToCart = document.querySelectorAll(".add-to-cart");
-    var hideShoppingCart = document.getElementById("hide-shopping-cart");
-    var headerCartButton = document.getElementById("cart-header-button");
-    var cartItemsWrapper = document.querySelector(".cart-items-wrapper");
-    var emptyCartMsg = document.getElementById("empty-cart");
+// Object containing selected items
+var cartItems = {
+    "selectedItems": [],
+    "total_price": 0
+}
 
-    var plusButton = document.querySelectorAll(".plusButton");
+// Add item on click on "Add to Cart" button
+for (var i = 0; i < addToCart.length; i++){
+    addToCart[i].addEventListener("click", function(e){
+        e.preventDefault();
+        addItem(this);
+        shoppingCart.style.display = "block";
+        /*this.setAttribute("disabled", true);*/
+    });
+}
 
+// Remove item on click on "Remove" button
+document.addEventListener("click", function() {
+    // Is this a good practice ? I have a feeling that the answer is no..;
+    var deleteItems = document.querySelectorAll(".delete-item");
+    var minusButton = document.querySelectorAll(".minusButton");
 
-    // Add item on click on "Add to Cart" button
-    for (var i = 0; i < addToCart.length; i++){
-        addToCart[i].addEventListener("click", function(e){
+    if(deleteItems.length == 0) {
+        emptyCartMsg.style.display = "block";
+    }
+    for (var i = 0; i < deleteItems.length; i++){
+        deleteItems[i].addEventListener("click", function(e){
             e.preventDefault();
-            addItem(this);
-            shoppingCart.style.display = "block";
-            this.setAttribute("disabled", true);
+            removeItem(this);
         });
     }
+});
 
-    // Remove item on click on "Remove" button
-    document.addEventListener("click", function() {
-        // Is this a good practice ? I have a feeling that the answer is no..;
-        var deleteItems = document.querySelectorAll(".delete-item");
-        var minusButton = document.querySelectorAll(".minusButton");
+// Hide shopping cart
+hideShoppingCart.addEventListener("click", function(){
+    shoppingCart.style.display = "none";
+});
 
-        if(deleteItems.length == 0) {
-            emptyCartMsg.style.display = "block";
-        }
-        for (var i = 0; i < deleteItems.length; i++){
-            deleteItems[i].addEventListener("click", function(e){
-                e.preventDefault();
-                removeItem(this);
-            });
-        }
-    });
-
-    // Hide shopping cart
-    hideShoppingCart.addEventListener("click", function(){
+// Toggle shopping cart
+headerCartButton.addEventListener("click", function(){
+    if(shoppingCart.style.display == "block") {
         shoppingCart.style.display = "none";
-    });
-
-    // Toggle shopping cart
-    headerCartButton.addEventListener("click", function(){
-        if(shoppingCart.style.display == "block") {
-            shoppingCart.style.display = "none";
-        } else {
-            shoppingCart.style.display = "block";
-        }
-    });
+    } else {
+        shoppingCart.style.display = "block";
+    }
+});
 
 
+// Function for adding items
+function addItem(item) {
+    var itemName = item.parentElement.parentElement.children[1].innerHTML;
+    var itemPrice = item.parentElement.children[0].children[1].innerHTML;
+    var itemImage = item.parentElement.parentElement.children[0].children[0].attributes[0].textContent;
+    var itemDescription = item.parentElement.parentElement.children[2].innerHTML;
 
-    // Function for adding items
-    function addItem(item) {
-        var itemPrice = item.parentElement.children[0].children[1].innerHTML;
-        var itemImage = item.parentElement.parentElement.children[0].children[0].attributes[0].textContent;
-        var itemName = item.parentElement.parentElement.children[1].innerHTML;
-        var itemDescription = item.parentElement.parentElement.children[2].innerHTML;
+
+    if(!isInCart(itemName)) {
 
         var div_node_cart_item = document.createElement("div");
         div_node_cart_item.className = "cart-item";
@@ -140,36 +144,130 @@ window.onload = function() {
         div_node_cart_item.append(div_node_price_wrapper);
 
         cartItemsWrapper.append(div_node_cart_item);
-
-        emptyCartMsg.style.display = "none";
+        // Push Items in cartItems object
+        cartItems.selectedItems.push({
+            "itemName": itemName,
+            "itemPrice": parseInt(itemPrice),
+            "itemImage": itemImage,
+            "itemDescription": itemDescription,
+            "itemQty": 1,
+            "itemTotalPrice": parseInt(itemPrice),
+        })
+    } else {
+        var itemInCart = find(itemName);
+        for(var i = 0; i < cartItems.selectedItems.length; i++){
+            if(cartItems.selectedItems[i].itemName == itemName){
+                cartItems.selectedItems[i].itemQty += 1;
+                cartItems.selectedItems[i].itemTotalPrice = cartItems.selectedItems[i].itemPrice * cartItems.selectedItems[i].itemQty;
+                itemInCart.children[2].children[1].value++;
+            }
+        }
     }
 
+    updateTotalPrice(cartItems);
 
+    // Hide message "Your shopping cart is empty"
+    emptyCartMsg.style.display = "none";
 }
 
 function removeItem(item){
     var itemParent = item.parentElement.parentElement.parentElement;
     itemParent.removeChild(item.parentElement.parentElement);
+    var itemName =  item.parentElement.parentElement.children[1].children[0].innerHTML;
+
+    for(var i = 0; i < cartItems.selectedItems.length; i++){
+        if(cartItems.selectedItems[i].itemName == itemName){
+            cartItems.selectedItems.splice(i);
+        }
+    }
 }
 
-// Increment input quantity
+// Decrement input quantity
 function decrement(item){
     var value = item.parentElement.children[1].value;
+    var itemName =  item.parentElement.parentElement.children[1].children[0].innerHTML;
 
     if (value > 1){
         value--;
         item.parentElement.children[1].value = value;
+
+        for(var i = 0; i < cartItems.selectedItems.length; i++){
+            if(cartItems.selectedItems[i].itemName == itemName){
+                cartItems.selectedItems[i].itemQty -= 1;
+                cartItems.selectedItems[i].itemTotalPrice = cartItems.selectedItems[i].itemPrice * cartItems.selectedItems[i].itemQty;
+            }
+        }
     }
     else {
         removeItem(item);
     }
+
+    updateTotalPrice(cartItems);
 }
 
 // Increment input quantity
 function increment(item){
     var value = item.parentElement.children[1].value;
+    var itemName =  item.parentElement.parentElement.children[1].children[0].innerHTML;
 
     value++;
     item.parentElement.children[1].value = value;
 
+    for(var i = 0; i < cartItems.selectedItems.length; i++){
+        if(cartItems.selectedItems[i].itemName == itemName){
+            cartItems.selectedItems[i].itemQty += 1;
+            cartItems.selectedItems[i].itemTotalPrice = cartItems.selectedItems[i].itemPrice * cartItems.selectedItems[i].itemQty;
+        }
+    }
+
+    updateTotalPrice(cartItems);
+}
+
+// Check if item is already in cart
+function isInCart(item){
+    var inCart = false;
+
+    for(var i = 0; i < cartItems.selectedItems.length; i++){
+        if(cartItems.selectedItems[i].itemName == item){
+            inCart = true;
+        }
+    }
+
+    return inCart;
+}
+
+function find(itemName){
+    var found = false;
+    var itemsInCart = document.querySelector(".cart-items-wrapper");
+
+    for(var i = 0; i < itemsInCart.children.length; i++){
+        if(itemsInCart.children[i].children[1].children[0].innerHTML == itemName) {
+            found = itemsInCart.children[i];
+        }
+    }
+    return found;
+}
+
+function updateTotalPrice(cartItems){
+    var totalPriceDisplay = document.getElementById("total-price");
+    var grandTotalDisplay = document.getElementById("grand-total");
+    var shippingFeeDisplay = document.getElementById("shipping-fee");
+    var totalPrice = 0;
+    var shippingFee = 12;
+
+    for(var i = 0; i < cartItems.selectedItems.length; i++){
+        totalPrice = totalPrice + cartItems.selectedItems[i].itemTotalPrice;
+    }
+
+    totalPriceDisplay.innerHTML = totalPrice;
+
+    if(totalPrice > 100) {
+        shippingFeeDisplay.className = "line-through";
+        grandTotalDisplay.innerHTML = totalPrice;
+    } else {
+        grandTotalDisplay.innerHTML = totalPrice + shippingFee;
+        shippingFeeDisplay.className = "";
+    }
+
+    return totalPrice;
 }
